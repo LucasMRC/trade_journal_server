5167137751ca6071d741d061ea742f1a54dd0267
lucas <lucas.reynacordoba22@gmail.com>
2022-04-14 11:39:44 -0300

    Base: Creates full route

---
+++ src/modules/base/index.ts
@@ -0,0 +1,10 @@
+
+import { BaseEntity } from './models/base.entity';
+// import { BaseService } from './service/base.service';
+// import { BaseRepository } from './repositories/base.repository';
+
+export {
+    BaseEntity
+    // BaseService,
+    // BaseRepository
+};

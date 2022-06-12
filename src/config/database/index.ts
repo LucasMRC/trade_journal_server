f4fbba4e2dd28f5a5d9a2161f5adf4c50be5be41
lucas <lucas.reynacordoba22@gmail.com>
2022-06-12 08:25:53 -0300

    Adds pass to database, fixes eslint

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -19,6 +19,7 @@ export const connectionConfig: ConnectionOptions = {
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     namingStrategy: new SnakeNamingStrategy(),
     entities: [
